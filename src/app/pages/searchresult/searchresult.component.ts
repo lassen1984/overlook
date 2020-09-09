18155764e38b897a14475284b2bf419dda631740
lassen1984 <stighofflassen@hotmail.com>
2020-09-09 10:18:28 +0200

    added sorting by alpha & number

--- src/app/pages/searchresult/searchresult.component.ts
+++ src/app/pages/searchresult/searchresult.component.ts
@@ -11,6 +11,9 @@ export class SearchresultComponent implements OnInit {
 
 searchResult: any
 
+reverseSearch = true;
+reverseId = true;
+
   constructor(private http: FetchserviceService, private route: ActivatedRoute) { }
 
   async ngOnInit(): Promise<void> {
@@ -22,7 +25,39 @@ console.log(keyword);
 this.searchResult = await this.http.getSearchResults(keyword).toPromise();
 this.searchResult = this.searchResult.items
 console.log(this.searchResult);
+}
 
+sortByNumber() {
+  if (this.reverseId) {
+    this.searchResult.sort((a,b) => a.id - b.id);
+  } else {
+    this.searchResult.sort((a,b) => b.id - a.id);
   }
+  this.reverseId = this.reverseId ? false : true
+}
 
+sortByName(){
+  if(this.reverseSearch){
+    this.searchResult.sort((a,b) => {
+    console.log(a.title);
+
+    if(b.title > a.title) return -1;
+
+    if(a.title > b.title) return 1;
+    return 0;
+  });
+
+  }else{
+    this.searchResult.sort((a,b) => {
+      console.log(a.title);
+
+      if(b.title > a.title) return 1;
+
+      if(a.title > b.title) return -1;
+
+      return 0;
+    });
+  }
+  this.reverseSearch = this.reverseSearch ? false : true
+  }
 }
